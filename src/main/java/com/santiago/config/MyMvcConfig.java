839828fd0079ae44e7c3d52235f6a2328223b8a5
santiago <1242255968@qq.com>
2019-11-06 13:22:19 +0800

    删除员工的功能完成 上一步修改员工其实post请求没有生效 这一步中在配置类中加了过滤器post请求和delete请求才能生效 我也不知道springboot自动配置的HiddenHttpMehtodFilter为什么没有生效

--- src/main/java/com/santiago/config/MyMvcConfig.java
+++ src/main/java/com/santiago/config/MyMvcConfig.java
@@ -2,12 +2,17 @@ package com.santiago.config;
 
 import com.santiago.component.LoginHandlerInterceptor;
 import com.santiago.component.MyLocaleResolver;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
 
 //@EnableWebMvc
 @Configuration
@@ -36,10 +41,21 @@ public class MyMvcConfig implements WebMvcConfigurer {
                 registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
                         .excludePathPatterns("/index.html", "/", "/user/login","/asserts/**");
             }
+
         };
         return adapter;
     }
-
+    //hiddenHttpMethodFilter
+    @Bean
+    public FilterRegistrationBean timeFilter() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        HiddenHttpMethodFilter myFilter = new HiddenHttpMethodFilter();
+        registrationBean.setFilter(myFilter);
+        ArrayList<String> urls = new ArrayList<>();
+        urls.add("/*");//配置过滤规则
+        registrationBean.setUrlPatterns(urls);
+        return registrationBean;
+    }
     @Bean
     public LocaleResolver localeResolver(){
         return new MyLocaleResolver();
--- src/main/java/com/santiago/controller/LoginController.java
+++ src/main/java/com/santiago/controller/LoginController.java
@@ -15,7 +15,7 @@ public class LoginController {
     public String login(@RequestParam("username") String username,
                         @RequestParam("password") String password,
                         Map<String, Object> map, HttpSession session){
-        if(!StringUtils.isEmpty(username) && "123456".equals(password)){
+        if(!StringUtils.isEmpty(username) && "123".equals(password)){
             //登录成功
             session.setAttribute("loginUser", username);
             return "redirect:/main.html";
